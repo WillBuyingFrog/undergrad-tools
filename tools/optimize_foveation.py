33281cac664cef8a549048efe5e3727f3db921d1
WillBuyingFrog <froggywch@gmail.com>
2024-03-15 01:21:44 +0800

    First version of the optimization algorithm. Wait for migrating.

--- tools/optimize_foveation.py
+++ tools/optimize_foveation.py
@@ -2,13 +2,17 @@ import torch
 import argparse
 import random
 import math
+import os
 from utils.logger import Logger
 from utils.visualize import visualize_ann_result
 
+
+OVERLAP_TRICK = True
+
 def calculate_overlap_area(x, y, tlwh, fovea_width, fovea_height):
     # 中央凹区域的左上角坐标
-    fovea_left = x - fovea_width / 2
-    fovea_top = y - fovea_height / 2
+    fovea_left = x
+    fovea_top = y
 
     # 中央凹区域的右下角坐标
     fovea_right = fovea_left + fovea_width
@@ -39,6 +43,16 @@ def calculate_coverage(x, y, tlwh, fovea_width, fovea_height):
     overlap_area = calculate_overlap_area(x, y, tlwh, fovea_width, fovea_height)
     target_area = tlwh[2] * tlwh[3]
     coverage = overlap_area / target_area
+
+    if OVERLAP_TRICK:
+        # 鉴于目标追踪器抽取特征的特性，能完整覆盖到整个目标比最大化覆盖率之和更重要，
+        # 应当在确保完整覆盖的情况下最大化覆盖率。
+        # 原始算法仅将所有覆盖率相加，不能体现确保完整覆盖目标的重要性，容易出现每个目标“雨露均沾”却都没有完整包围目标的情况
+        # 这种情况下，目标追踪器抽取特征不稳定（一部分清晰一部分不清晰），提升效果或许有限
+        # 所以当OVERLAP_TRICK开启时，本函数返回的coverage值不再表示原始的覆盖率，而是表示更广义上的描述中央凹区域覆盖本目标的“得分”
+        # 对覆盖率不足1的情况施加惩罚，使得原始覆盖率不足1时，“得分”迅速下降
+        coverage = coverage ** 7
+
     return coverage
 
 def total_coverage(x, y, tlwhs, fovea_width, fovea_height):
@@ -49,7 +63,7 @@ def total_coverage(x, y, tlwhs, fovea_width, fovea_height):
 
 def simulated_annealing(tlwhs, fovea_width, fovea_height, img_width, img_height, init_x=None, init_y=None):
 
-    logger = Logger('annealing_test.txt')
+    logger = Logger('results/annealing_test.txt')
 
     # 定义初始状态
     if init_x is not None:
@@ -64,9 +78,9 @@ def simulated_annealing(tlwhs, fovea_width, fovea_height, img_width, img_height,
     current_score = total_coverage(current_x, current_y, tlwhs, fovea_width, fovea_height)
 
     # 温度调度参数
-    initial_temp = 1000
+    initial_temp = 100.0
     final_temp = 1
-    alpha = 0.95
+    alpha = 0.96
     temp = initial_temp
 
     # 记录循环轮次
@@ -77,11 +91,11 @@ def simulated_annealing(tlwhs, fovea_width, fovea_height, img_width, img_height,
         counter += 1
 
         # 随机选择新的状态（邻域函数）
-        next_x = current_x + random.uniform(-10, 10)  # 调整这个步长大小以适应具体问题
-        next_y = current_y + random.uniform(-10, 10)
+        next_x = current_x + random.uniform(-75, 75)  # 调整这个步长大小以适应具体问题
+        next_y = current_y + random.uniform(-75, 75)
 
         # 不断随机采样，直到采样得到新的合法坐标
-        new_pos_eff = 10.0
+        new_pos_eff = 75.0
         while next_x + fovea_width > img_width or next_x <= 0:
             new_pos_eff *= 1.2
             new_pos_eff = min(new_pos_eff, fovea_width)
@@ -95,7 +109,7 @@ def simulated_annealing(tlwhs, fovea_width, fovea_height, img_width, img_height,
         next_score = total_coverage(next_x, next_y, tlwhs, fovea_width, fovea_height)
 
         # 计算接受概率
-        accept_probability = math.exp((current_score - next_score) / temp)
+        accept_probability = math.exp((next_score - current_score) / temp)
 
         logger.write_log(f'Epoch {counter} - Temp: {temp:.2f}, Current score: {current_score:.2f}, Next score: {next_score:.2f}, Accept probability: {accept_probability:.2f}')
 
@@ -110,7 +124,7 @@ def simulated_annealing(tlwhs, fovea_width, fovea_height, img_width, img_height,
     logger.write_log(f'Final position: x={current_x:.2f}, y={current_y:.2f}, score={current_score:.2f}')
     logger.close()  
 
-    return current_x, current_y
+    return current_x, current_y, current_score
 
 
 if __name__ == '__main__':
@@ -123,6 +137,7 @@ if __name__ == '__main__':
     parser.add_argument('--fovea_width', default=400, type=int, help='中央凹区域宽度')
     parser.add_argument('--fovea_height', default=300, type=int, help='中央凹区域高度')
     parser.add_argument('--exp_epochs', default=1, type=int, help='算法迭代次数')
+    parser.add_argument('--clear_previous', default=True, type=bool, help='是否清除之前的可视化结果')
 
     args = parser.parse_args()
     img_width = args.img_width
@@ -130,15 +145,25 @@ if __name__ == '__main__':
     fovea_width = args.fovea_width
     fovea_height = args.fovea_height
     exp_epochs = args.exp_epochs
+    clear_previous = args.clear_previous
+
+    if clear_previous:
+        if os.path.exists('results'):
+            # 删除其中所有的.jpg文件
+            for file in os.listdir('results'):
+                if file.endswith('.jpg'):
+                    os.remove(os.path.join('results', file))
 
 
     # 示例: 假设有一个目标列表
     tlwhs = [
-        torch.tensor([200, 200, 50, 50]),  # [top, left, width, height]
-        torch.tensor([100, 100, 50, 80]),
-        torch.tensor([80, 80, 50, 50]),
-        torch.tensor([130, 130, 50, 50]),
-        torch.tensor([180, 180, 20, 20])
+        torch.tensor([80, 100, 35, 100]),
+        torch.tensor([100, 120, 35, 100]),
+        torch.tensor([120, 150, 35, 100]),
+        torch.tensor([150, 180, 35, 100]),
+        torch.tensor([190, 210, 35, 100]), 
+        torch.tensor([500, 100, 35, 100]), 
+        torch.tensor([550, 100, 50, 150]), 
         # ... 更多目标
     ]
 
@@ -159,21 +184,28 @@ if __name__ == '__main__':
         print(x.grad, y.grad)
     elif args.algo_type == 'annealing':
         
-        avg_x, avg_y = 0, 0
+        avg_x, avg_y, total_score = 0, 0, 0.0
         results = []
         for i in range(args.exp_epochs):
-            x, y = simulated_annealing(tlwhs, fovea_width, fovea_height, img_width, img_height)
-            avg_x += x
-            avg_y += y
-            results.append((x, y))
+            x, y, current_score = simulated_annealing(tlwhs, fovea_width, fovea_height, img_width, img_height,
+                                       init_x=210, init_y=210)
+            results.append((x, y, current_score))
             print(f'Epoch {i+1} - Simulated annealing result x={x}, y={y}')
-            visualize_ann_result(tlwhs, x, y, fovea_width, fovea_height, img_width, img_height, save_path='results/', img_name=f'test_{i}.jpg',)
+            if i % 5 == 0:
+                visualize_ann_result(tlwhs, x, y, fovea_width, fovea_height, img_width, img_height, save_path='results/', img_name=f'test_{i}.jpg',)
 
         
-        avg_x /= args.exp_epochs
-        avg_y /= args.exp_epochs
+        # 按照每个轮次返回的score进行加权平均
+        for i in range(args.exp_epochs):
+            x, y, score = results[i]
+            avg_x += x * score
+            avg_y += y * score
+            total_score += score
+        avg_x /= (args.exp_epochs * total_score)
+        avg_y /= (args.exp_epochs * total_score)
 
         print(f'Average position: x={avg_x:.2f}, y={avg_y:.2f}')
+        visualize_ann_result(tlwhs, avg_x, avg_y, fovea_width, fovea_height, img_width, img_height, save_path='results/', img_name=f'test_avg.jpg',)
 
         
         # print(f'Simulated annealing result x={x}, y={y}')
--- tools/utils/visualize.py
+++ tools/utils/visualize.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 
 
 # 将单次模拟退火的结果画成图
-def visualize_ann_result(tlwhs, x, y, fovea_width, fovea_height, img_width, img_height,
+def visualize_ann_result(tlwhs, fovea_x, fovea_y, fovea_width, fovea_height, img_width, img_height,
                          save_path='results/', img_name='test.jpg'):
-    # 设定画布大小为img_width * img_height
-    plt.figure(figsize=(img_width / 100, img_height / 100))
+    
 
     # 画出目标框
     for tlwh in tlwhs:
         x, y, w, h = tlwh
         plt.plot([x, x + w, x + w, x, x], [y, y, y + h, y + h, y], 'r')
-    # 中央凹区域的左上角坐标为(x,y)，右下角坐标为(x+fovea_width, y+fovea_height)
-    plt.plot([x, x + fovea_width, x + fovea_width, x, x], [y, y, y + fovea_height, y + fovea_height, y], 'b')
+    # 中央凹区域的左上角坐标为(fovea_x,fovea_y)，右下角坐标为(fovea_x+fovea_width, fovea_y+fovea_height)
+    plt.plot([fovea_x, fovea_x + fovea_width, fovea_x + fovea_width, fovea_x, fovea_x],
+             [fovea_y, fovea_y, fovea_y + fovea_height, fovea_y + fovea_height, fovea_y], 'b')
+
+    # 设置当前画的图的横轴画到img_width，纵轴最大值画到img_height
+    plt.xlim(0, img_width)
+    plt.ylim(0, img_height)
 
     # 保存图像到save_path/img_name
     plt.savefig(os.path.join(save_path, img_name))
