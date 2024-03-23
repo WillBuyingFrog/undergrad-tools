bbe869336ae07b9dd0fdb88f6508e89d538c8b44
Will Frog <froggywch@gmail.com>
2024-03-23 17:17:06 +0800

    Migrate calculate_roi from FairMOT experiments

--- tools/calculate_roi_production.py
+++ tools/calculate_roi_production.py
@@ -5,6 +5,7 @@ import os
 
 from frogutils.logger import Logger
 
+
 class FrogROI:
     def __init__(self, image_width, image_height, init_image_path,
                  region_scale=0.1, pixel_change_threshold=50):
@@ -15,7 +16,11 @@ class FrogROI:
         # 需要持续更新的比较基准（engram）是和current_image形状相同的numpy array，初始值全部置零
         self.engram = np.zeros((self.image_height, self.image_width, 3), dtype=np.float32)
         # self.engram_factor = np.array([5, 3, 2.5, 2, 1.5, 1.25, 1, 0.75, 0.5, 0.25])
-        self.engram_factor = np.array([10, 2.5, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.2, 0.1])
+        # self.engram_factor = np.array([10, 2.5, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.2, 0.1])
+        # Inspired from "Predictive properties of visual adaptation"
+        self.engram_factor = np.array([10, 5, 2.5, 1, 0.5, 0.2, 0.2, -0.2, -0.5, -1])
+        # The engram factors below doesnt work :(
+        # self.engram_factor = np.array([2.5, 2.5, 5.0, 7.5, 7.5, 7.5, 5.0, 1.5, 0.5, 0.2])
         self.region_scale = region_scale
         self.pixel_change_threshold = pixel_change_threshold
         # 用于计算比较基准的图片
@@ -110,6 +115,15 @@ def visualize_rois(img, img0, engram, roi_tlwhs, result_path='results/', file_ma
     cv2.imwrite(os.path.join(result_path, f"{file_marker}_origin.jpg"), img0)
     cv2.imwrite(os.path.join(result_path, f"{file_marker}_engram.jpg"), engram)
 
+def visualize_all_regions(img, roi_tlwhs, target_tlwhs, result_path='results/', file_marker='test'):
+    for roi in roi_tlwhs:
+        cv2.rectangle(img, (roi[0], roi[1]), (roi[0]+roi[2], roi[1]+roi[3]), (0, 255, 0), 2)
+    for target in target_tlwhs:
+        print(f'Drawing target: {target}')
+        top_left = (int(target[0]), int(target[1]))
+        right_bottom = (int(target[0] + target[2]), int(target[1] + target[3]))
+        cv2.rectangle(img, top_left, right_bottom, (255, 0, 0), 2)
+    cv2.imwrite(os.path.join(result_path, f"{file_marker}_all_regions.jpg"), img)
 
 if __name__ == '__main__':
 
